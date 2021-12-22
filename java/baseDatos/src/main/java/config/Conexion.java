9621b1340391c9e71a9978a92d3a85f03c5db5ce
Barbyvel <barbyvel@yahoo.com.ar>
2021-12-22 16:35:25 -0300

    proyecto final

--- java/baseDatos/src/main/java/config/Conexion.java
+++ java/baseDatos/src/main/java/config/Conexion.java
@@ -2,23 +2,43 @@
 package config;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Conexion {
-    public String driver = "com.mysql.jdbc.Driver";
-    public Connection getConection() throws ClassNotFoundException{
+    public String driver = "com.mysql.cj.jdbc.Driver";
+    public Connection getConection() {
         Connection conexion=null;
         try{
             Class.forName(driver);
             conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/comision2160",
-                    "admin","123456");
+                    "root","");
         }catch(SQLException e){
             System.out.println(e.toString());
+        }catch (ClassNotFoundException ex){
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
         }
         return conexion;
      }
-    public static void main(String [] args) throws ClassNotFoundException{
+    public static void main(String [] args) throws SQLException, ClassNotFoundException{
         Connection conexion = null;
         Conexion con = new Conexion();
         conexion = con.getConection();
+        PreparedStatement ps;
+        ResultSet rs;
+        
+        ps = conexion.prepareStatement("SELECT * FROM participante");
+        rs = ps.executeQuery();
+        while(rs.next()){
+            int id = rs.getInt("id");
+            String nombre = rs.getString("nombres");
+            String apellido = rs.getString("apellidos");
+            String email = rs.getString("email");
+            int telefono = rs.getInt("telefono");
+            System.out.println("id: " + id + "\n Nombre: " + nombre +
+                    "\n Apellido: " + apellido + "\n Email: " + email
+            + "\n Telefono: " + telefono);
+        }
+           
     }
 }
